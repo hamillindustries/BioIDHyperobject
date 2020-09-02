fb69a97c5444a4369878614a987a9137ad34168c
Yi-Wen Lin <bongiovi015@gmail.com>
2020-09-02 12:10:48 +0100

    build

---
+++ dev/build/precache-manifest.a8fd11adff08552d12d87adefdab9c30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704b792ef66da31e4442632bf8f20bc6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d18f4c247d376d1a9ba1",
+    "url": "./static/css/main.dd92d0ce.chunk.css"
+  },
+  {
+    "revision": "da618a42b4e1b64cf36b",
+    "url": "./static/js/2.f7b9fb3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f7b9fb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18f4c247d376d1a9ba1",
+    "url": "./static/js/main.f5bb512f.chunk.js"
+  },
+  {
+    "revision": "f38bd2e966721891df10",
+    "url": "./static/js/runtime-main.33b62b0d.js"
+  },
+  {
+    "revision": "8efd50d574d934fc72857901c2575cc6",
+    "url": "./static/media/bg.8efd50d5.jpg"
+  }
+]);
